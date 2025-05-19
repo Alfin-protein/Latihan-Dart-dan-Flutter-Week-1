6f1644e88494e798337efe15126908b5692204dd
Alfin <alfingamping@gmail.com>
2025-05-19 09:44:59 +0700

    Latihan Hari 1: Variabel dan Tipe Data

---
+++ day1.dart
@@ -0,0 +1,17 @@
+void main() {
+  var nama = "Alfin";
+  var umur = 19;
+  var berat = 57.5;
+  var tinggi = 175.5;
+  var sukaCoding = true;
+  var perempuan = true;
+  var hobi = ['coding','gaming','membaca','workout'];
+
+print("Nama = $nama");
+print("Umur = $umur");
+print("Berat Badan = $berat kg");
+print("Tinggi Badan = $tinggi cm");
+print("Suka coding? = $sukaCoding");
+print("Jenis kelamin = ${perempuan ? 'Laki-laki' : 'Perempuan'}");
+print("Hobi = $hobi");
+}
