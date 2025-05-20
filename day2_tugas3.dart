6e01c613f5d730b735411a26e29ad603427c1a80
Alfin <alfingamping@gmail.com>
2025-05-20 11:57:22 +0700

    Hapus file lama setelah restrukturisasi folder

--- day2_tugas3.dart
+++
@@ -1,8 +0,0 @@
-void main() {
-  var pi = 3.14;
-  var jariJari = 7.0;
-
-  var luasLingkaran = pi * jariJari * jariJari;
-
-  print("Luas lingkarang adalah $luasLingkaran");
-}
--- day2_tugas4.dart
+++
@@ -1,8 +0,0 @@
-void main() {
-  var nilaiMatematika = 80;
-  var nilaiFisika = 90;
-
-  var isNilaiMatematikaLebihKecil = nilaiMatematika < nilaiFisika;
-  
-  print("Apakah nilai Matematika lebih kecil dari nilai Fisika? $isNilaiMatematikaLebihKecil");
-}
